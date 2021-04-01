36d6c35392cf0b39ae80f55eef586ad57913bfd9
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-04-01 12:14:37 -0400

    working on navbar

---
+++ src/components/NavTabs.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function NavTabs() {
+  const location = useLocation();
+
+  return (
+    <nav
+      className="navbar navbar-expand-sm navbar-light"
+      style="background-color: #464646"
+    >
+      <li className="navbar-brand text-white-50">
+        <Link to="/" className={location.pathname === "/"}>
+          Eric Freyer
+        </Link>
+      </li>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link
+              className="nav-link text-white-50"
+              className={location.pathname === "/"}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-item active">
+            <Link
+              className="nav-link text-white-50"
+              className={location.pathname === "/portfolio"}
+              id="portfolio"
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="nav-item active">
+            <Link
+              className="nav-link text-white-50"
+              className={location.pathname === "/contact"}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default NavTabs;
