c0e3e0a4b2dbf9a82b9fe0a7ec825ebbba6c6007
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-04-01 16:51:25 -0400

    homepage layout finished

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavTabs() {
   const location = useLocation();
@@ -9,41 +11,59 @@ function NavTabs() {
       className="navbar navbar-expand-sm navbar-light"
       style={{ backgroundColor: "#464646"}}>
       <li className="navbar-brand text-white-50">
-        <Link to="/" className={location.pathname === "/"}>
+        <a href="/" className="nav-link text-white-50" {...location.pathname === "/"}>
           Eric Freyer
-        </Link>
+        </a>
       </li>
-      <div className="collapse navbar-collapse" id="navbarNav">
+      <div className="collapse navbar-collapse navbarNav">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <Link
-              className="nav-link text-white-50"
-              className={location.pathname === "/"}
+            <a href="/"
+              className="nav-link text-white-50" {...location.pathname === "/"}
             >
               Home
-            </Link>
+            </a>
           </li>
           <li className="nav-item active">
-            <Link
-              className="nav-link text-white-50"
-              className={location.pathname === "/portfolio"}
-              id="portfolio"
+            <a href="/portfolio"
+              className="nav-link text-white-50 portfolio" {...location.pathname === "/portfolio"}
             >
               Portfolio
-            </Link>
+            </a>
           </li>
           <li className="nav-item active">
-            <Link
-              className="nav-link text-white-50"
-              className={location.pathname === "/contact"}
+            <a href="/contact"
+              className="nav-link text-white-50" {...location.pathname === "/contact"}
             >
               Contact
-            </Link>
+            </a>
           </li>
         </ul>
       </div>
     </nav>
   );
 }
+
+$(document).ready(function(){
+    $(".fadeIn").hide();
+$(".portfolio").click(function(e) {
+    $(".fadeIn").show()
+    if ($('.fadeIn').css('opacity') === 0) {
+        $('.fadeIn').css('opacity', 1);
+    }
+    else {
+        $('.fadeIn').css('opacity', 0);
+    }
+    if ($(".middleDiv").css('opacity') === 1){
+        $(".middleDiv").css("opacity", 0)
+    }
+    else {
+        $("middleDiv").css('opacity', 1);
+    }
+    e.preventDefault();
+    var linkUrl = $(this).attr('href');
+    setTimeout(function(url) { window.location = url; }, 8000, linkUrl);
+});
+});
 
 export default NavTabs;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,26 +1,24 @@
 import React from "react";
-import $ from 'jquery'
-import IMG_3760 from './images/IMG_3760.JPG'
-import GitHub from './images/GitHub-Mark.png'
-import gmail from './images/gmail.webp'
-import Linkedin from './images/Linkedin_symbol_transparent.png'
+import $ from "jquery";
+import IMG_3760 from "./images/IMG_3760.JPG";
+import GitHub from "./images/GitHub-Mark.png";
+import gmail from "./images/gmail.webp";
+import Linkedin from "./images/Linkedin_symbol_transparent.png";
 
-
-
-function Home() {
-
-
+function Home(props) {
   return (
     <div>
-      <div className="fadeIn" id="fadeDiv">
+      <div className="fadeIn fadeDiv">
         <h1>Okay...Here we go!</h1>
       </div>
       <div
-        id="middleDiv"
-        className="container sm border border-dark w-50 p-3"
-        style={{marginTop: "5%"}} style={{backgroundColor: "rgba(211, 197, 252, 0.116)"}}
+        className="container sm border border-dark w-50 p-3 middleDiv"
+        style={{
+          marginTop: "5%",
+          backgroundColor: "rgba(211, 197, 252, 0.116)",
+        }}
       >
-        <h1 style={{textAlign: "center"}}>About Me</h1>
+        <h1 style={{ textAlign: "center" }}>About Me</h1>
         <div className="row">
           <div className="col-sm-6 h-100 d-inline-block">
             <img
@@ -30,73 +28,80 @@ function Home() {
               className="rounded
           float-left img-fluid"
               alt="Eric"
-              style={{height: "auto"}}
+              style={{ height: "auto" }}
             />
-              <div className="row">
-                <div
-                  className="col-sm-12 d-inline-block"
-                  style={{textAlign: "center", paddingTop: "2%", paddingLeft: "10%"}}
+            <div className="row">
+              <div
+                className="col-sm-12 d-inline-block"
+                style={{
+                  textAlign: "center",
+                  paddingTop: "2%",
+                  paddingLeft: "10%",
+                }}
+              >
+                <ul
+                  className="list-group list-group-horizontal"
+                  style={{ listStyleType: "none" }}
                 >
-                  <ul
-                    className="list-group list-group-horizontal"
-                    style={{listStyleType: "none"}}
-                  >
-                    <li>
-                      <a href="mailto: ericcfreyer@gmail.com">
-                        <img
-                          src={gmail}
-                          width="180%"
-                          height="100%"
-                          className="rounded float-left"
-                          alt="Gmail"
-                          className="img-fluid img-thumbnail"
-                        />
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
+                  <li>
+                    <a href="mailto: ericcfreyer@gmail.com">
+                      <img
+                        src={gmail}
+                        style={{ width: "120%", height: "80%" }}
+                        className="rounded float-left img-fluid img-thumbnail"
+                        alt="Gmail"
+                      />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      alt="Linkedin"
+                      className="rounded float-left"
+                      href="https://www.linkedin.com/in/eric-freyer-977719168/"
+                    >
+                      <img
+                        src={Linkedin}
+                        style={{
+                          height: "25%",
+                          width: "25%",
+                          background: "none",
+                        }}
                         alt="Linkedin"
-                        className="rounded float-left"
-                        href="https://www.linkedin.com/in/eric-freyer-977719168/"
-                      >
-                        <img
-                          src={Linkedin}
-                          style={{height: "25%", width: "25%"}}
-                        />
-                      </a>
-                    </li>
-                    <li style={{paddingRight: "7%"}}>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        alt="Github"
-                        className="rounded float-left"
-                        href="https://github.com/ericfreyer"
-                      >
-                        <img
-                          src={GitHub}
-                          style={{height: "120%", width: "120%"}}
-                        />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+                      />
+                    </a>
+                  </li>
+                  <li style={{ paddingRight: "7%" }}>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      alt="Github"
+                      className="rounded float-left"
+                      href="https://github.com/ericfreyer"
+                    >
+                      <img
+                        src={GitHub}
+                        style={{ height: "120%", width: "120%" }}
+                        alt="GitHub"
+                      />
+                    </a>
+                  </li>
+                </ul>
               </div>
-            <div
-              className="col-sm-6 border text-justify overflow-auto align-justified d-flex justify-content-center d-flex align-items-center"
-              style={{fontFamily: 'Noto Serif, serif', fontSize: '20px'}}
-            >
-              Hello! My name is Eric Freyer. I lived for five years in Montana
-              where I received my BS in biology. I currently make a living
-              working as a freelance art handler for various museums and
-              galleries in and around the metropolitan area. Most notably I have
-              worked for the American Museum of Natural History, the MOMA, the
-              Met, and in Andy Warhol's studio. I am now studying to become a
-              full-stack web developer and have grown a love for front-end work
-              and design.
             </div>
+          </div>
+          <div
+            className="col-sm-6 border text-justify overflow-auto align-justified d-flex justify-content-center d-flex align-items-center"
+            style={{ fontFamily: "Noto Serif, serif", fontSize: "20px" }}
+          >
+            Hello! My name is Eric Freyer. I lived for five years in Montana
+            where I received my BS in biology. I currently make a living working
+            as a freelance art handler for various museums and galleries in and
+            around the metropolitan area. Most notably I have worked for the
+            American Museum of Natural History, the MOMA, the Met, and in Andy
+            Warhol's studio. I am now studying to become a full-stack web
+            developer and have grown a love for front-end work and design.
           </div>
         </div>
       </div>
@@ -104,26 +109,30 @@ function Home() {
   );
 }
 
-$(document).ready(function(){
-    $(".fadeIn").hide();
-$("#portfolio").click(function(e) {
-    $(".fadeIn").show()
-    if ($('.fadeIn').css('opacity') == 0) {
-        $('.fadeIn').css('opacity', 1);
+$(document).ready(function () {
+  $(".fadeIn").hide();
+  $(".portfolio").click(function (e) {
+    $(".fadeIn").show();
+    if ($(".fadeIn").css("opacity") === 0) {
+      $(".fadeIn").css("opacity", 1);
+    } else {
+      $(".fadeIn").css("opacity", 0);
     }
-    else {
-        $('.fadeIn').css('opacity', 0);
-    }
-    if ($("#middleDiv").css('opacity') == 1){
-        $("#middleDiv").css("opacity", 0)
-    }
-    else {
-        $("middleDiv").css('opacity', 1);
+    if ($(".middleDiv").css("opacity") === 1) {
+      $(".middleDiv").css("opacity", 0);
+    } else {
+      $(".middleDiv").css("opacity", 1);
     }
     e.preventDefault();
-    var linkUrl = $(this).attr('href');
-    setTimeout(function(url) { window.location = url; }, 8000, linkUrl);
-});
+    var linkUrl = $(this).attr("href");
+    setTimeout(
+      function (url) {
+        window.location = url;
+      },
+      8000,
+      linkUrl
+    );
+  });
 });
 
 export default Home;
