2f29bed568ec44ce74f3be8b6aad08f23a441888
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-04-06 16:44:08 -0400

    deploying

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
           <NavTabs />
 
           <Route exact path="" component={Home} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/Portfolio" component={Portfolio} />
+          <Route exact path="/Contact" component={Contact} />
 
         </div>
       
